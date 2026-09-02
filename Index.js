a23d88a380cb6a0d37ac528818cf6d1ce6768abe
alvesdasilvaenrique71-ops <alvesdasilvaenrique71@gmail.com>
2026-09-02 18:29:19 -0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,4 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once("ready", () => { console.log("Bot online!"); });
+client.login("SEU_TOKEN_AQUI");
